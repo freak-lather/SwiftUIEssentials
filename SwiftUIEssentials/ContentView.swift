374defd68ed0029accb2fdb5e7eee4e233d91f17
freak_lather <ajaysinghlather@gmail.com>
2020-04-04 14:16:19 +0800

    Initial Commit

---
+++ SwiftUIEssentials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIEssentials
+//
+//  Created by ajay lather on 04/04/20.
+//  Copyright © 2020 Ajay Lather. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
