01fcabd94a1dc497f3eac0a75db9feb8524a54a7
freak_lather <ajaysinghlather@gmail.com>
2020-04-05 12:40:10 +0800

    first commit

---
+++ SwiftUIEssentials/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  SwiftUIEssentials
+//
+//  Created by ajay lather on 04/04/20.
+//  Copyright © 2020 Ajay Lather. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ SwiftUIEssentials/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  SwiftUIEssentials
+//
+//  Created by ajay lather on 05/04/20.
+//  Copyright © 2020 Ajay Lather. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
